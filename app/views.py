a058db6fc53479785c503584819e7c5fbd7957f7
Nicole Carlson <nicole@parsingscience.com>
2015-08-06 07:33:21 -0500

    Better setup for next random student

--- app/views.py
+++ app/views.py
@@ -2,21 +2,27 @@ from flask import render_template
 from app import app
 from flask import request
 import random
-import numpy as np
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 
 @app.route('/', methods=['POST'])
 def student_list():
-    text = request.form['students-list']
-    names=text.split(',')
-    print len(names)
-    temp_array= np.random.choice(len(names), len(names), replace=False)
-    print temp_array
-    temp_index=0
-    return render_template('index.html', studentlist=text, randomstudent=names[temp_array[temp_index]])
+  text = request.form['students-list']
+  names=text.split(',')
+
+  names=[str(item) for item in names]
+
+  # Save a master class list for later
+  master_list=names
+
+  temp_student=random.sample(names, 1)[0]
+  
+  #Remove current temp student so student won't be chosen twice.
+  names.remove(temp_student)
+  
+  return render_template('index.html', studentlist=text, randomstudent=temp_student)
 
