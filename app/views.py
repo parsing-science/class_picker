0d7d7ccaef041a839773a9b5f135d30472733c01
Nicole Carlson <nicole@parsingscience.com>
2015-08-06 08:08:16 -0500

    Allow teacher to go through list of all students once

--- app/views.py
+++ app/views.py
@@ -11,18 +11,36 @@ def index():
 
 @app.route('/', methods=['POST'])
 def student_list():
-  text = request.form['students-list']
-  names=text.split(',')
+	print request.form
+	global names
+	global master_list
+	global text
+	if "my-form" in request.form:
+		text = request.form['students-list']
+		names=text.split(',')
 
-  names=[str(item) for item in names]
+		names=[str(item) for item in names]
 
-  # Save a master class list for later
-  master_list=names
+		# Save a master class list for later
+		master_list=names
 
-  temp_student=random.sample(names, 1)[0]
-  
-  #Remove current temp student so student won't be chosen twice.
-  names.remove(temp_student)
-  
-  return render_template('index.html', studentlist=text, randomstudent=temp_student)
+		temp_student=random.sample(names, 1)[0]
+
+		#Remove current temp student so student won't be chosen twice.
+		names.remove(temp_student)
+		print names
+
+		return render_template('index.html', studentlist=text, randomstudent=temp_student)
+	elif "new-student" in request.form:
+		print names
+		try:
+			temp_student=random.sample(names, 1)[0]
+
+			#Remove current temp student so student won't be chosen twice.
+			names.remove(temp_student)
+
+			return render_template('index.html', studentlist=text, randomstudent=temp_student)
+		except:
+			return render_template('index.html', studentlist=text, randomstudent="No more students!")
+
 
